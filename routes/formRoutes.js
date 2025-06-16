bc1869068473449d0c93c4ede1941f5665c04f48
Divya Singh <divyasingh0612@gmail.com>
2025-06-16 02:45:12 -0700

    code

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -5,7 +5,7 @@ const path = require("path");
 const formController = require("../controllers/formController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.get("/get-forms", authMiddleware, formController.getAllForms);
+
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "uploads/"),
