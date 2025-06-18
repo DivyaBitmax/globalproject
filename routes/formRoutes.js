8d5724066245778f21aab010cd943b8c455412a4
Divya Singh <divyasingh0612@gmail.com>
2025-06-17 22:33:01 -0700

    Updated code here

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -8,7 +8,7 @@ const formController = require("../controllers/formController");
 
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "uploads/"),
+  destination: (req, file, cb) => cb(null, "uploads/documents/"),
   filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname)
 });
 const upload = multer({ storage, limits: { fileSize: 5 * 1024 * 1024 } });
