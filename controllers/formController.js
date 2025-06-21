6b930640d0807aab6cfa4ecaa3d57da41d240302
Divya Singh <divyasingh0612@gmail.com>
2025-06-20 23:12:57 -0700

    new email id

--- controllers/formController.js
+++ controllers/formController.js
@@ -34,13 +34,25 @@ exports.submitForm = async (req, res) => {
     await newForm.save();
 
     // STEP 3: Send Email
+    // const transporter = nodemailer.createTransport({
+    //   service: "gmail",
+    //   auth: {
+    //     user: process.env.EMAIL_USER,    
+    //     pass: process.env.EMAIL_PASS     
+    //   }
+    // });
+
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: process.env.SMTP_HOST,
+      port: Number(process.env.SMTP_PORT),
+      secure: false, // true for SSL (port 465)
       auth: {
-        user: process.env.EMAIL_USER,    
-        pass: process.env.EMAIL_PASS     
-      }
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
     });
+
+
     const mailOptions = {
       from: `GlobalProjects <${process.env.EMAIL_USER}>`,
       to: email,
