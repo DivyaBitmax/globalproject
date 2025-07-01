7fcf9c853b2fbf1b8d075d44f004f8022a3ac7c4
Divya Singh <divyasingh0612@gmail.com>
2025-07-01 13:44:47 +0530

    new code

--- utils/cloudinaryStorage.js
+++ utils/cloudinaryStorage.js
@@ -1,19 +1,40 @@
+// // utils/cloudinaryStorage.js
+
+
+// const multer = require('multer');
+// const { CloudinaryStorage } = require('multer-storage-cloudinary');
+// const cloudinary = require('../config/cloudinary');
+
+// const storage = new CloudinaryStorage({
+//   cloudinary,
+//   params: async (req, file) => {
+//     return {
+//       folder: 'global-projects',
+//       resource_type: file.mimetype.startsWith('application') ? 'raw' : 'image',
+//     };
+//   },
+// });
+
+// module.exports = multer({ storage });
+
+
+
 // utils/cloudinaryStorage.js
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('../config/cloudinary');
+const path = require('path');
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: async (req, file) => {
+    const ext = path.extname(file.originalname); // get .jpg, .png, .pdf
     return {
       folder: 'global-projects',
       resource_type: file.mimetype.startsWith('application') ? 'raw' : 'image',
+      public_id: `${Date.now()}-${file.fieldname}${ext}`, // 👈 force extension
     };
   },
 });
 
 module.exports = multer({ storage });
-
-
-
