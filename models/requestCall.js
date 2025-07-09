c288980ea94432a25eb5d0ded6f58467cfe9ec6d
Divya Singh <divyasingh0612@gmail.com>
2025-07-09 12:24:15 +0530

    graphAPI

--- models/requestCall.js
+++ models/requestCall.js
@@ -4,6 +4,8 @@ const requestCallSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
+    remarks: { type: String }, // ✅ Add this line
+  
 }, { timestamps: true });
 
 module.exports = mongoose.model("RequestCall", requestCallSchema);
